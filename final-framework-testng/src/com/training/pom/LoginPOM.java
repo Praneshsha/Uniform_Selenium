835591de89e0c632c234a09fb024ec105a323fab
ps853j <ps853j@DESKTOP-S9N87U7.fios-router.home>
2019-12-03 00:40:15 -0500

    PRANESH SARAVANAMUTHU TestCase_UFM_007

--- final-framework-testng/src/com/training/pom/LoginPOM.java
+++ final-framework-testng/src/com/training/pom/LoginPOM.java
@@ -1,5 +1,6 @@
 package com.training.pom;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -13,14 +14,33 @@ public class LoginPOM {
 		PageFactory.initElements(driver, this);
 	}
 	
-	@FindBy(id="login")
+	@FindBy(xpath="//a[@title=\"My Account\"]")
+	private WebElement myaccbtn;
+	
+	@FindBy(id="input-email")
 	private WebElement userName; 
 	
-	@FindBy(id="password")
+	@FindBy(id="input-password")
 	private WebElement password;
 	
-	@FindBy(id="formLogin_submitAuth")
+	@FindBy(partialLinkText="Login")
 	private WebElement loginBtn; 
+	
+	
+	@FindBy(xpath="//input[@value='Login']")
+	private WebElement logintomyaccount; 
+	
+	@FindBy(partialLinkText="Change your password")
+	private WebElement changepasswordlink;
+	
+	@FindBy(id="input-password")
+	private WebElement firstpwd;
+	
+	@FindBy(id="input-confirm")
+	private WebElement confirmpwd;
+	
+	@FindBy(xpath="//input[@value=\"Continue\"]")
+	private WebElement continuebtn;
 	
 	public void sendUserName(String userName) {
 		this.userName.clear();
@@ -35,4 +55,29 @@ public class LoginPOM {
 	public void clickLoginBtn() {
 		this.loginBtn.click(); 
 	}
+	
+	public void Myaccountbtn() {
+		this.myaccbtn.click();
+	}
+	public void siginside() {
+			this.logintomyaccount.click();
+		
+	}
+	public void changepwdlink() {
+		this.changepasswordlink.click();
+	}
+	public void firstpwd(String firstpassword) {
+		this.firstpwd.clear();
+		this.firstpwd.sendKeys(firstpassword);
+	}
+	public void confirmpwd(String confirmpassword) {
+		this.confirmpwd.clear();
+		this.confirmpwd.sendKeys(confirmpassword);
+	}
+	
+	public void continuebutton() {
+		this.continuebtn.click();
+	}
+
+	
 }
--- final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
+++ final-framework-testng/tests/com/training/sanity/tests/LoginTests.java
@@ -4,7 +4,10 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By.ById;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -42,14 +45,29 @@ public class LoginTests {
 	
 	@AfterMethod
 	public void tearDown() throws Exception {
-		Thread.sleep(1000);
+		Thread.sleep(3000);
 		driver.quit();
 	}
 	@Test
-	public void validLoginTest() {
-		loginPOM.sendUserName("admin");
-		loginPOM.sendPassword("admin@123");
-		loginPOM.clickLoginBtn(); 
+	public void validLoginTest() throws InterruptedException {
+		loginPOM.Myaccountbtn();
+		loginPOM.clickLoginBtn();
+		loginPOM.sendUserName("Praneshsha@hotmail.com");
+		loginPOM.sendPassword("Selenium@123");
+		//loginPOM.Mainloginbtn();
+		loginPOM.siginside();
+		Thread.sleep(3000);
+		loginPOM.changepwdlink();
+		//WebElement firstpassword = driver.findElement(By.id("input-password")).sendKeys("sneha");
+		//System.out.println("password is:" +firstpassword);
+		loginPOM.firstpwd("sneha");
+		Thread.sleep(3000);
+		loginPOM.confirmpwd("mneha");
+		//System.out.println("Confirm Password is: " +confirmpwd);
+		Thread.sleep(3000);
+		loginPOM.continuebutton();
+		System.out.println("Password confirmation does not match password!");
+		
 		screenShot.captureScreenShot("First");
 	}
 }
